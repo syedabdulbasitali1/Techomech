aa284f4afba94856bce857f56d0569787ac75bd3
Abdul Basit Ali <syedabdulbasitali1@gmail..com>
2025-02-18 15:13:31 -0800

    update

---
+++ script.js
@@ -0,0 +1,149 @@
+// Register ScrollTrigger with GSAP
+gsap.registerPlugin(ScrollTrigger);
+
+// Initialize Locomotive Scroll
+// const locoScroll = new LocomotiveScroll({
+//   el: document.querySelector('[data-scroll-container]'),
+//   smooth: true,
+// });
+
+// Sync ScrollTrigger with Locomotive Scroll
+// locoScroll.on('scroll', ScrollTrigger.update);
+// ScrollTrigger.scrollerProxy('[data-scroll-container]', {
+//   scrollTop(value) {
+//     return arguments.length
+//       ? locoScroll.scrollTo(value, 0, 0)
+//       : locoScroll.scroll.instance.scroll.y;
+//   },
+//   getBoundingClientRect() {
+//     return {
+//       top: 0, left: 0,
+//       width: window.innerWidth,
+//       height: window.innerHeight,
+//     };
+//   },
+//   pinType: 'transform',
+// });
+
+// Refresh ScrollTrigger
+// ScrollTrigger.addEventListener('refresh', () => locoScroll.update());
+// ScrollTrigger.refresh();
+
+
+
+
+
+let tl = gsap.timeline()
+
+gsap.from(".top-bar .icon", {
+    y: -100,
+    opacity: 0,
+    duration: 2,
+    ease: "power3.out"
+})
+
+gsap.from(".social", {
+    x: "100%",
+    opacity: 0,
+    duration: 2,
+    ease: "power3.out"
+})
+gsap.from(".social .navigations i", {
+    opacity: 0,
+    duration: 2,
+    delay: 0,
+    ease: "power2"
+})
+gsap.from(".box", {
+    x: "-100%",
+    duration: 1,
+    ease: "power3.out",
+})
+
+gsap.from(".box .content", {
+    opacity: 0,
+    duration: 2,
+    delay: 1,
+    ease: "power3.out"
+    // duration: 1,
+    // ease: "power3.out"
+})
+gsap.from("nav", {
+    x: "100%",
+    opacity: 0,
+    duration: 2,
+    ease: "power4.out"
+})
+
+
+  // Mobile Scroll Effect
+function mobileGsap() {
+    gsap.to(".services .container", {
+        x: "-40%", // Use 'x' for horizontal translation
+        scrollTrigger: {
+          trigger: ".services ",
+        //   scroller: "[data-scroll-container]",
+          scroller: "main",
+          start: "top 0%",
+          end: "top -100%",
+          scrub: 2,
+          pin: true,
+          markers: true,
+        },
+      });
+  }
+
+  // Viewport Check
+  if (window.innerWidth <= 768) {
+    mobileGsap();
+  } else {
+    gsap.to(".services .container", {
+        x: "-20%", // Use 'x' for horizontal translation
+        scrollTrigger: {
+          trigger: ".services ",
+        //   scroller: "[data-scroll-container]",
+          scroller: "main",
+          start: "top 0%",
+          end: "top -100%",
+          scrub: 2,
+          pin: true,
+          markers: true,
+        },
+      });
+  }
+
+//   window.addEventListener('resize', () => {
+//     ScrollTrigger.getAll().forEach(trigger => trigger.kill());
+//     if (window.innerWidth <= 768) {
+//       mobileGsap();
+//     } else {
+//       desktopGsap();
+//     }
+//   });
+
+
+
+// gsap.fromTo(".services .container",
+//     { x: '-50%', opacity: 1},
+//     {
+//       x: '20%', opacity: 1,
+//       duration: 3,
+//       ease: 'power2.out',
+//       scrollTrigger: {
+//         trigger: '.services',
+//         // scroller: '[data-scroll-container]',
+//         scroller: 'body',
+//         start: 'top 70%',
+//         end: 'bottom 20%',
+//         scrub: true,
+//         markers: true,
+//         pin:true,
+//       }
+//     }
+//   );
+
+//   locoScroll.on('scroll', ScrollTrigger.update);
+
+// ScrollTrigger.addEventListener('refresh', () => locoScroll.update());
+
+// ScrollTrigger.refresh();
